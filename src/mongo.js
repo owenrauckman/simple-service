52f96a5e1dd8d08f5306f57079000efefd8a4338
owenrauckman <owenrauckman@gmail.com>
2019-06-15 19:02:13 -0500

    adding .env

--- src/mongo.js
+++ src/mongo.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 module.exports = async () => {
   try{
-    await mongoose.connect(process.ENV.MONGO_DB_STRING, { useNewUrlParser: true, dbName: "help" })
+    await mongoose.connect(process.env.MONGO_DB_STRING, { useNewUrlParser: true, dbName: "help" })
   } catch(e){
     console.log(e)
   }
